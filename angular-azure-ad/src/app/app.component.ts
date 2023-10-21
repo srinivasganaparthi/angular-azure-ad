44d479a7e33308ba4e3f0b970a6bf4653f119cb7
srinivasganaparthi <ganaparthi.srinu@gmail.com>
2023-10-21 11:11:04 -0400

    Update app.component.ts

--- angular-azure-ad/src/app/app.component.ts
+++ angular-azure-ad/src/app/app.component.ts
@@ -1,10 +1,53 @@
+import { MsalService } from '@azure/msal-angular';
 import { Component } from '@angular/core';
+import { AuthenticationResult } from '@azure/msal-browser';
+import { HttpClient } from '@angular/common/http';
+import { PolicyClientService, PolicyModel } from './policy-client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-azure-ad';
+  title = 'My Microsoft Login- Example';
+
+  apiResponse: string | undefined;
+
+  constructor(private authService: MsalService, private http: HttpClient, private policyClientService: PolicyClientService) {
+
+  }
+  ngOnInit(): void {
+    this.authService.instance.handleRedirectPromise().then(res => {
+      if (res != null && res.account != null) {
+        this.authService.instance.setActiveAccount(res.account)
+      }
+    })
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.instance.getActiveAccount() != null
+  }
+
+  login() {
+    this.authService.loginPopup()
+      .subscribe((response: AuthenticationResult) => {
+        this.authService.instance.setActiveAccount(response.account);
+      });
+  }
+
+  logout() {
+    this.authService.logout()
+  }
+
+
+  private readonly baseURL = 'https://localhost:7193';
+
+  getPolicy() {
+    this.http.get<PolicyModel[]>(this.baseURL + "/api/Policy").subscribe(resp => {
+      this.apiResponse = JSON.stringify(resp)
+    })
+  }
 }
+
+
